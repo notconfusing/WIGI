2058b0ddb347f4d9c316bb3f393bf6145c148652
notconfusing <isalix@gmail.com>
2015-08-03 16:43:21 -0700

    reorganized and added worldmap function

--- gender-index-processing-standalone.py
+++ gender-index-processing-standalone.py
@@ -10,13 +10,46 @@ from collections import defaultdict
 import time
 import shutil
 
+#CONSTANSTS AND LOCATIONS
 java_min_int = -2147483648
-
+snap = '/home/maximilianklein/snapshot_data/'
+java_place = '/home/maximilianklein/Wikidata-Toolkit/wdtk-examples/results'
+pobs_map = json.load(open('helpers/aggregation_maps/pobs_map.json','r'))
+country_map = pandas.DataFrame.from_csv('helpers/aggregation_maps/country_maps.csv')
+ethnic_group_map = json.load(open('helpers/aggregation_maps/mechanical_turk/ethnic_groups_map.json','r'))
+citizenship_map = json.load(open('helpers/aggregation_maps/mechanical_turk/citizenship_map.json','r')) 
 #Tranforming QIDs into English labels.
 enwp = pywikibot.Site('en','wikipedia')
 wikidata = enwp.data_repository()
 
+#GLOBAL Memory
 retrieved = dict()
+unknown_countries = list()
+
+###
+#HELPERS
+###
+def map_pob(qids):
+    try:
+        country_list = pobs_map[qids[0]]
+        country = country_list[0] #assumption
+        return country
+    except:
+        unknown_countries.append(qids)
+        return None
+
+def map_country(qids):
+    country = map_pob(qids)
+    culture = country_map.ix[country]['culture_name']
+    return culture
+
+def map_wrapper(m):
+    def return_fun(qids):
+        try:
+            return m[qids[0]]
+        except KeyError:
+            return None
+    return return_fun
 
 def english_label(qid):
     if type(qid) is float:
@@ -42,128 +75,98 @@ def engify_labels(df):
     df.columns = labels
     return df
 
-snap = '/home/maximilianklein/snapshot_data/'
-java_place = '/home/maximilianklein/Wikidata-Toolkit/wdtk-examples/results'
-snapshot_dates = os.listdir(java_place)
-latest_file_name = max(snapshot_dates)
-latest_date = '-'.join(latest_file_name.split('.')[0].split('-')[-3:])
-latest = os.path.join(java_place,latest_file_name)
-#cp file over here and make property index
-new_snap_location = os.path.join(snap,latest_date)
-if not os.path.exists(new_snap_location):
-    os.makedirs(new_snap_location)
-copy_dest = os.path.join(new_snap_location,latest_file_name)
-shutil.copyfile(latest, copy_dest)
+def organise_snaps():
+    snapshot_dates = os.listdir(java_place)
+    latest_file_name = max(snapshot_dates)
+    latest_date = '-'.join(latest_file_name.split('.')[0].split('-')[-3:])
+    latest = os.path.join(java_place,latest_file_name)
+    #cp file over here and make property index
+    new_snap_location = os.path.join(snap,latest_date)
+    if not os.path.exists(new_snap_location):
+        os.makedirs(new_snap_location)
+    copy_dest = os.path.join(new_snap_location,latest_file_name)
+    shutil.copyfile(latest, copy_dest)
+    property_index_dir = os.path.join(new_snap_location, 'property_indexes')
+    if not os.path.exists(property_index_dir):
+        os.makedirs(property_index_dir)
+    return copy_dest
 
-df = pandas.read_csv(copy_dest, na_values=[java_min_int])
+def split_columns(df):
+    def split_column(q_str):
+        if type(q_str) is float:
+            if numpy.isnan(q_str):
+                return [q_str] #returning this way so we can gurantee that column contains list
+        if type(q_str) is str:
+            qs = q_str.split('|')
+            return qs[:-1] #cos the format will always end with a |
 
-def split_column(q_str):
-    if type(q_str) is float:
-        if numpy.isnan(q_str):
-            return [q_str] #returning this way so we can gurantee that column contains list
-    if type(q_str) is str:
-        qs = q_str.split('|')
-        return qs[:-1] #cos the format will always end with a |
-
-for column in ['gender', 'ethnic_group', 'citizenship', 'place_of_birth', 'site_links']:
-    column_plural = column+'s'
-    df[column_plural] = df[column].apply(split_column)
-    del df[column]
+    for column in ['gender', 'ethnic_group', 'citizenship', 'place_of_birth', 'site_links']:
+        column_plural = column+'s'
+        df[column_plural] = df[column].apply(split_column)
+        del df[column]
+    return df
 
 ###
 #CULTURES
 ###
-pobs_map = json.load(open('helpers/aggregation_maps/pobs_map.json','r'))
-country_map = pandas.DataFrame.from_csv('helpers/aggregation_maps/country_maps.csv')
+def make_culture(df):
+    #order is important because it determines the preference we will use
+    col_map_fun = zip(['ethnic_groups', 'citizenships', 'place_of_births'],
+                      [map_wrapper(ethnic_group_map),map_wrapper(citizenship_map), map_country])
 
-ethnic_group_map = json.load(open('helpers/aggregation_maps/mechanical_turk/ethnic_groups_map.json','r'))
-citizenship_map = json.load(open('helpers/aggregation_maps/mechanical_turk/citizenship_map.json','r')) 
+    mismatch = pandas.DataFrame()
+    def determine_culture(row):
+        culture = None
+        for col, map_fun in col_map_fun:
+            val = row[col]
+            guess = map_fun(val)
+            if (culture is not None) and (guess is not None):
+                if culture != guess:
+                    mismatch.append(row,ignore_index=True)
+            if guess:
+                culture = guess    
+        return str(culture).lower() if culture else culture #to return None properly
 
-unknown_countries = list()
-
-def map_pob(qid):
-    if not type(qid) is str:
-        return None
-    else:
-        try:
-            country_list = pobs_map[qid]
-        except:
-            unknown_countries.append(qid)
-            return None
-        if len(country_list) == 0:
-            return None
-        else:
-            country = country_list[0] #assumption
-            culture = country_map.ix[country]['culture_name']
-            return culture
-
-def map_wrapper(m):
-    def return_fun(qid):
-        try:
-            return m[qid]
-        except KeyError:
-            return None
-    return return_fun
-
-
-
-#order is important because it determines the preference we will use
-col_map_fun = zip(['ethnic_groups', 'citizenships', 'place_of_births'],
-                  [map_wrapper(ethnic_group_map),map_wrapper(citizenship_map), map_pob])
-
-mismatch = pandas.DataFrame()
-def determine_culture(row):
-    culture = None
-    for col, map_fun in col_map_fun:
-        val = row[col]
-        #print val
-        if isinstance(val,list):
-            val = val[0]
-        guess = map_fun(val)
-        if (culture is not None) and (guess is not None):
-            if culture != guess:
-                mismatch.append(row,ignore_index=True)
-        if guess:
-            culture = guess    
-    return str(culture).lower() if culture else culture #to return None properly
-
-
-df['culture'] = df.apply(lambda x: determine_culture(x), axis=1)
+    df['culture'] = df.apply(lambda x: determine_culture(x), axis=1)
+    return df
     
 ###
 #WORLD MAP
 ###
-
-modrecs = df.copy(deep=True)
-
-modrecs['country'] = modrecs.apply(lambda x: map_pob(x), axis=1)
-map_cit = map_wrapper(citizenship_map)
-modrecs['citizenship'] = modrecs.apply(lambda x: map_cit(x), axis=1)
-cdf = modrecs[['country','citizenship','gender']]
-
-def combine_economy(row):
-    cit = row['citizenship']
-    cunt = row['country']
-    return cit if cit else cunt
-cdf['Economy_qid'] = cdf.apply(combine_economy,axis=1)
-edf = cdf[cdf['Economy_qid'].apply(lambda x: x is not None)]
-bios_count = len(edf)
-
-edf['Economy'] = edf['Economy_qid'].apply(english_label)
+def make_world_map(df):
+    df['country'] = df.apply(lambda x: map_pob(x['place_of_births']), axis=1)
+    #print(df.head())
+    map_cit = map_wrapper(citizenship_map)
+    df['citizenship'] = df.apply(lambda x: x['citizenships'][0], axis=1)
+    df['gender'] = df.apply(lambda x: x['genders'][0], axis=1)
+    cdf = df[['country','citizenship','gender']]
+    print(cdf.head())
 
 
-country_perc = defaultdict(dict)
-country_groups= edf.groupby(by='Economy')
+    def combine_economy(row):
+        cit = row['citizenship']
+        cunt = row['country']
+        return cit if cit else cunt
+    cdf['Economy_qid'] = cdf.apply(lambda x: combine_economy(x),axis=1)
+    edf = cdf[cdf['Economy_qid'].apply(lambda x: x is not None)]
+    bios_count = len(edf)
 
-for country, group in country_groups:
-    nonmale = group[group['gender'] != 'Q6581097']['gender'].count()
-    total = group['gender'].count()
-    nm_perc = nonmale / float(total)
-    country_perc[country]['Economy'] = country #for later on joining
-    country_perc[country]['Score'] = nm_perc #for later on joining
-    country_perc[country]['total']= total
+    edf['Economy'] = edf['Economy_qid'].apply(english_label)
+    print(edf.head())
 
-wdf = pd.DataFrame.from_dict(country_perc, orient='index')
+    country_perc = defaultdict(dict)
+    country_groups= edf.groupby(by='Economy')
+
+    for country, group in country_groups:
+        nonmale = group[group['gender'] != 'Q6581097']['gender'].count()
+        total = group['gender'].count()
+        nm_perc = nonmale / float(total)
+        country_perc[country]['Economy'] = country #for later on joining
+        country_perc[country]['Score'] = nm_perc #for later on joining
+        country_perc[country]['total']= total
+
+    wdf = pandas.DataFrame.from_dict(country_perc, orient='index')
+    return wdf
 
 ###
 #REINDEX
@@ -204,16 +207,26 @@ def make_reindex(df):
     gender_dfs = {param: pandas.DataFrame.from_dict(gender_param[param], orient='columns') for param in params}
     return gender_dfs
 
-reindexed_dfs = make_reindex(df)
-
-for param, gender_df in reindexed_dfs.iteritems():
-    engify_labels(gender_df)
-    property_index_dir = os.path.join(new_snap_location, 'property_indexes')
-    if not os.path.exists(property_index_dir):
-        os.makedirs(property_index_dir)
+def save_property_index(param, df):
     filename = '%s/%s-index.csv' % (property_index_dir, param)
     filepoint = open(filename, 'w')
     filepoint.write(gender_df.to_csv())
     filepoint.close()
 
 
+def save_reindex(reindexed_dfs):
+    for param, gender_df in reindexed_dfs.iteritems():
+        engify_labels(gender_df)
+        save_property_index(param, gender_df)
+
+
+if __name__ == '__main__':
+    copy_dest = organise_snaps()
+    df = pandas.read_csv(copy_dest, na_values=[java_min_int])
+    df = split_columns(df)
+    #df = make_culture(df)
+    #reindexed_dfs = make_reindex(df)
+    #save_reindex(reindexed_dfs)
+
+    wdf = make_world_map(df)
+    save_property_index('worldmap', wdf)
