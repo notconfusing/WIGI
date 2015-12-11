f8a496e8a20347e5fc3ef359095dff6a01329c61
Harsh Gupta <gupta.harsh96@gmail.com>
2015-12-11 17:31:53 +0530

    Fix for python 3
    Signed-off-by: Harsh Gupta <gupta.harsh96@gmail.com>

--- scripts/bls_wikidata_matchup.py
+++ scripts/bls_wikidata_matchup.py
@@ -5,7 +5,7 @@ def split_func(string):
     string = string.replace('\n', ', ')
     cats = string.split(',')
     cats = [cat.strip() for cat in cats]
-    cats = filter(lambda x: len(x) > 0, cats)
+    cats = list(filter(lambda x: len(x) > 0, cats))
     return cats
 
 
@@ -13,10 +13,14 @@ bls_wd = pd.read_csv('../data/bls_wd.csv')
 wd = pd.read_csv('../data/fm_occupation.csv')
 bls = pd.read_csv('../data/labelled_bls_occupations.csv')
 bls_wd = bls_wd[~bls_wd.wd_occupation.isnull()]
+bls_wd['bls_total'] = bls_wd['bls_total'].apply(lambda x: float(x))
+bls_wd['bls_p_women'] = bls_wd['bls_p_women'].apply(lambda x: float(x))
+
 bls_wd['wd_occupation'] = bls_wd['wd_occupation'].apply(lambda x: split_func(x))
+
 
 bls_wd['wd_total'] = bls_wd['wd_occupation'].apply(lambda x: wd.loc[wd['category'].isin(x)].sum().total)
 bls_wd['wd_women'] = bls_wd['wd_occupation'].apply(lambda x: wd.loc[wd['category'].isin(x)].sum().female)
 # bls_wd['wd_male'] = bls_wd['wd_occupation'].apply(lambda x: wd.loc[wd['category'].isin(x)].sum().male)
 bls_wd['wd_p_women'] = 100*bls_wd['wd_women']/bls_wd['wd_total']
-bls_wd.to_csv('../data/bls_wd.csv')
+bls_wd.to_csv('../data/bls_wd_matchup.csv')
