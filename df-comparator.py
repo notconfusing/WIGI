0a7dc2b0a0b5fae3e9d51642239686b89a27da3e
notconfusing <isalix@gmail.com>
2016-01-05 11:01:55 -0800

    sample tex file

--- df-comparator.py
+++ df-comparator.py
@@ -2,23 +2,36 @@ import pandas
 import os
 
 
-def diff(fa, fb):
+def changes_between(fa, fb):
     dfa = pandas.DataFrame.from_csv(fa)
     dfb = pandas.DataFrame.from_csv(fb)
 
     removed_columns = dfa.columns.difference(dfb.columns)
     added_columns = dfb.columns.difference(dfa.columns)
     
-    change = dfb - dfa
-    return change
+    change_df = dfb - dfa
+    return change_df
 
 
-def find_folders():
-    snapfolder = '/home/maximilianklein/snapshot_data'
-    dates = os.listdir(snapfolder)
+def find_dirs():
+    snapdir = '/home/maximilianklein/snapshot_data'
+    dates = os.listdir(snapdir)
     sdates = sorted(dates)
     latest = sdates[-1]
     prev = sdates[-2]
-    print os.path.join(os.path.join(snapfolder,latest),'property_indexes')
-
-find_folders()
+    latest_dir = os.path.join(os.path.join(snapdir,latest),'property_indexes')
+    prev_dir = os.path.join(os.path.join(snapdir,prev),'property_indexes')
+    latest_files = os.listdir(latest_dir)
+    prev_files = os.listdir(prev_dir)
+    changedir = os.path.join(latest_dir,'changes-since-{}'.format(prev))
+    if not os.path.exists(changedir):
+        os.makedirs(changedir)
+   
+    
+    for ind_file in latest_files:
+        if ind_file in prev_files:
+            p_f = os.path.join(prev_dir, ind_file)
+            l_f = os.path.join(latest_dir, ind_file)
+            change_df = changes_between(p_f, l_f)
+            
+find_dirs()
