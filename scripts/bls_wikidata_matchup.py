4670fd25156da86fc0483fab78aa734ecf58f584
Harsh Gupta <gupta.harsh96@gmail.com>
2016-01-08 20:47:29 +0530

    Save the datafile stagewise
    Signed-off-by: Harsh Gupta <gupta.harsh96@gmail.com>

--- scripts/bls_wikidata_matchup.py
+++ scripts/bls_wikidata_matchup.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 def split_func(string):
     string = string.replace('\n', ', ')
     cats = string.split(',')
@@ -12,21 +13,18 @@ bls_wd_match = pd.read_csv('../data/bls_wd.csv')
 wd = pd.read_csv('../data/fm_occupation.csv')
 
 
-def get_bls_wd(wd):
-    bls_wd = bls_wd_match.copy(deep=True)
-    # bls = pd.read_csv('../data/labelled_bls_occupations.csv')
-    bls_wd = bls_wd[~bls_wd.wd_occupation.isnull()]
-    bls_wd['bls_total'] = bls_wd['bls_total'].apply(lambda x: float(x))
+# def get_bls_wd(wd):
+bls_wd = bls_wd_match.copy(deep=True)
+# bls = pd.read_csv('../data/labelled_bls_occupations.csv')
+bls_wd = bls_wd[~bls_wd.wd_occupation.isnull()]
+bls_wd['bls_total'] = bls_wd['bls_total'].apply(lambda x: float(x))
 
-    # Reduce percentage in the range of 0 to 1
-    bls_wd['bls_p_women'] = bls_wd['bls_p_women'].apply(lambda x: float(x)/100)
+# Reduce percentage in the range of 0 to 1
+bls_wd['bls_p_women'] = bls_wd['bls_p_women'].apply(lambda x: float(x)/100)
 
-    bls_wd['wd_occupation'] = bls_wd['wd_occupation'].apply(lambda x: split_func(x))
+bls_wd['wd_occupation'] = bls_wd['wd_occupation'].apply(lambda x: split_func(x))
 
-
-    bls_wd['wd_total'] = bls_wd['wd_occupation'].apply(lambda x: wd.loc[wd['category'].isin(x)].sum().total)
-    bls_wd['wd_women'] = bls_wd['wd_occupation'].apply(lambda x: wd.loc[wd['category'].isin(x)].sum().female)
-    bls_wd['wd_p_women'] = bls_wd['wd_women']/bls_wd['wd_total']
-    bls_wd.to_csv('../data/bls_wd_matchup.csv')
-
-
+bls_wd['wd_total'] = bls_wd['wd_occupation'].apply(lambda x: wd.loc[wd['category'].isin(x)].sum().total)
+bls_wd['wd_women'] = bls_wd['wd_occupation'].apply(lambda x: wd.loc[wd['category'].isin(x)].sum().female)
+bls_wd['wd_p_women'] = bls_wd['wd_women']/bls_wd['wd_total']
+bls_wd.to_csv('../data/bls_wd_matchup.csv')
