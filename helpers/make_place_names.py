75025c0dfe97e82f847ac11c7a8686b57b712ccf
Max Klein <isalix@gmail.com>
2016-03-18 11:42:40 -0700

    opensym draft1

--- helpers/make_place_names.py
+++ helpers/make_place_names.py
@@ -1,8 +1,11 @@
 import pywikibot
 import json
+import sys
 
+infile, outfile = sys.argv[1], sys.argv[2]
 
-pobs = json.load(open('pobs_list.json','r'))
+pobs = json.load(open(infile,'r'))
+
 
 enwp = pywikibot.Site('en','wikipedia')
 wikidata = enwp.data_repository()
@@ -15,7 +18,8 @@ def is_or_has_country(qid):
     for pid, claimlist in claims.iteritems():
         if pid == 'P17':
             for claim in claimlist:
-                countries.append(claim.target.title()) #this is part of a country
+                if claim.target:
+                    countries.append(claim.target.title()) #this is part of a country
         if pid == 'P31':
             for claim in claimlist:
                     if claim.target.title() == 'Q6256':
@@ -27,9 +31,12 @@ place_country = dict()
 
 count=0
 for place in pobs: #1 because the first index is nan
-    place_country[place] = is_or_has_country(place)
-    count += 1
-    if count % 100 == 0:
-        print count
+    try:
+        place_country[place] = is_or_has_country(place)
+        count += 1
+        if count % 100 == 0:
+            print count
+    except:
+        print 'error on {}'.format(place)
 
-json.dump(place_country, open('pobs_map.json','w'))
+json.dump(place_country, open(outfile,'w'))
